e806ec1346b6666a912b6af120e263b057ffa48d
nijiko-ds <59920622+nijikos@users.noreply.github.com>
2023-06-05 11:38:08 +0700

    Move fakedb from web

---
+++ src/@fakedb/documentList.ts
@@ -0,0 +1,42 @@
+export const documents = [
+  {
+    id: "6f2077b7-46d2-4392-87ab-43c259d5b576",
+    display: "superstore sale.csv",
+  },
+  {
+    id: "f5c5ef43-8d63-49fd-a5e9-071287a1e55d",
+    display: "daily-min-temperatures.csv",
+  },
+  {
+    id: "9df6e248-894e-43d7-9f23-b8f4feab76d0",
+    display: "avocado.csv",
+  },
+  {
+    id: "cdc2c3be-5b34-4132-824e-8c7c011f6c14",
+    display: "uni_rank.csv",
+  },
+  {
+    id: "e61f5811-7a3a-4629-8d16-ec8330c8d7c3",
+    display: "tips.csv",
+  },
+  {
+    id: "827c4dcf-c620-48a7-98e1-5ff4c8e193eb",
+    display: "iris.csv",
+  },
+  {
+    id: "b78c8a2e-0ad6-4e0f-8e4a-1e0f17b5ae48",
+    display: "population_galicia.csv",
+  },
+  {
+    id: "b7218a2e-2de4-4e0f-8e4a-15ff17b5ae48",
+    display: "notARealFile.csv",
+  },
+];
+
+// #(superstore_sale.csv[f2cfba87-1587-4001-8fb0-802dc8cf0b6f])
+// #(daily-min-temperatures.csv[f2cfba87-1587-4001-8fb0-802dc8cf0b6f])
+// #(avocado.csv[f2cfba87-1587-4001-8fb0-802dc8cf0b6f])
+// #(uni_rank.csv[f2cfba87-1587-4001-8fb0-802dc8cf0b6f])
+// #(tips.csv[f2cfba87-1587-4001-8fb0-802dc8cf0b6f])
+// #(iris.csv[f2cfba87-1587-4001-8fb0-802dc8cf0b6f])
+// #(population_galicia.csv[f2cfba87-1587-4001-8fb0-802dc8cf0b6f])
