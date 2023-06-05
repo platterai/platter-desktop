17285cc7c77aa56d3b4125bb838a174526026a6b
nijiko-ds <59920622+nijikos@users.noreply.github.com>
2023-06-05 11:39:14 +0700

    cleanChartData from web

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -1,4 +1,6 @@
 import { appWindow, LogicalSize } from "@tauri-apps/api/window";
+import { isArray, omit } from "lodash";
+import { Datum, IBarchart } from "../types/app";
 
 export const anchorLink = (url: string, newTab: boolean) => {
   const link = document.createElement("a");
@@ -29,4 +31,23 @@ export const setWindowSize = (w: number, h: number) => {
   appWindow.setSize(new LogicalSize(w, h));
   appWindow.setMaxSize(new LogicalSize(w + 200, h + 200));
   appWindow.setMinSize(new LogicalSize(w - 200, h - 200));
+};
+
+export const cleanChartData = (_data: IBarchart) => {
+  let newData = _data?.data?.map((item: Datum) => {
+    const cleanItem =
+      item?.marker?.color && !isArray(item?.marker?.color)
+        ? omit(item, ["marker"])
+        : item;
+    return cleanItem;
+  });
+
+  let newLayout = omit(_data?.layout, ["coloraxis"]);
+
+  const result = {
+    data: newData,
+    layout: newLayout,
+  };
+
+  return result;
 };
