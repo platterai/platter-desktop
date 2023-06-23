a35cb03cc36b9a46ef799b0138cf7f8f428d90c0
nijiko-ds <59920622+nijikos@users.noreply.github.com>
2023-06-23 16:10:56 +0700

    Add appearance mode in preferences

--- src/components/Settings/Preferences/index.tsx
+++ src/components/Settings/Preferences/index.tsx
@@ -1,7 +1,10 @@
 import { Grid, GridItem } from "@chakra-ui/layout";
 import { Select } from "@chakra-ui/select";
+import { isEmpty } from "lodash";
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
+import useLocalStorage from "../../../hooks/useLocalStorage";
 import { setShowWelcome } from "../../../redux/slices/showWelcomeSlice";
 
 type PreferencesProps = {};
@@ -12,9 +15,18 @@ const Preferences = ({}: PreferencesProps) => {
     (state: any) => state.showWelcome.showWelcome
   );
 
+  const [colormode, setColormode, deleteColormode] =
+    useLocalStorage<string>("colormode");
+
+  useEffect(() => {
+    console.log({ colormode });
+    return () => {};
+  }, [colormode]);
+
   return (
     <div>
       <Grid templateColumns='repeat(4, 1fr)' gap={6} alignItems='center'>
+        {/* ============== Show welcome and hint */}
         <GridItem w='100%' colSpan={3}>
           <p>Show welcome and hint</p>
         </GridItem>
@@ -50,6 +62,34 @@ const Preferences = ({}: PreferencesProps) => {
           >
             <option value='true'>Yes</option>
             <option value='false'>No</option>
+          </Select>
+        </GridItem>
+
+        {/* ============== Appearance */}
+        <GridItem w='100%' colSpan={3}>
+          <p>Appearance</p>
+        </GridItem>
+        <GridItem w='100%' colSpan={1}>
+          <Select
+            variant='unstyled'
+            value={isEmpty(colormode) ? "light" : colormode}
+            sx={{
+              border: "1px solid var(--n-3) !important",
+              padding: "8px 16px",
+            }}
+            _focus={{
+              border: "1px solid var(--n-4) !important",
+            }}
+            _active={{
+              border: "1px solid var(--n-4) !important",
+            }}
+            onChange={(e) => {
+              console.log(`e.target.value`, { value: e.target.value });
+              setColormode(e.target.value);
+            }}
+          >
+            <option value='light'>Light</option>
+            <option value='dark'>Dark</option>
           </Select>
         </GridItem>
       </Grid>
